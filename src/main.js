07d41cb954f9314d8ffe88dae8d518a6a824161f
xyf960905 <381204378@qq.com>
2018-11-22 16:14:55 +0800

    首页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import VueRouter from 'vue-routery
+import "./assets/site/css/style.css";
+
+
+// Vue.use(VueRouter)
+
 
 Vue.config.productionTip = false
 
+// let routers=[
+
+// ]
+
+// let router=new VueRouter({
+//   routers
+// })
 new Vue({
   render: h => h(App),
+  // router
 }).$mount('#app')
